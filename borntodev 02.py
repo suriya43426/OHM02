5f20209accffaebfab8e49a101d89d8a28a02dfb
suriya chayatummagoon <42575704+suriya43426@users.noreply.github.com>
2019-04-10 22:42:15 +0700

    HW02

---
+++ borntodev 02.py
@@ -0,0 +1,15 @@
+def addNumber (x,y):
+    print(x+y)
+addNumber(60,500)
+
+def addNumber2 (x,y):
+    print(x-y)
+addNumber2(60,500)
+
+def addNumber3 (x,y):
+    print(x*y)
+addNumber3(60,500)
+
+def addNumber4 (x,y):
+    print(x/y)
+addNumber4(60,500)
